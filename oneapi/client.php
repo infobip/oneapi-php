60578e989162498a5475eecf881e5590d52fed74
Tomo Krajina <tkrajina@gmail.com>
2012-08-03 12:31:17 +0200

    subscribeToInboundMessagesNotifications serialization fix

--- oneapi/client.php
+++ oneapi/client.php
@@ -309,7 +309,7 @@ class SmsClient extends AbstractOneApiClient {
     public function subscribeToInboundMessagesNotifications($moSubscription) {
         $restUrl = $this->getRestUrl('/1/smsmessaging/inbound/subscriptions');
 
-        $params = $this->convertToJSON($moSubscription);
+        $params = Conversions::convertToJSON($moSubscription);
 
         list($isSuccess, $content) = $this->executePOST($restUrl, $params);
 
