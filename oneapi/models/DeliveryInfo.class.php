d7bca5b8679d0fafa35058e490a16aaa6a4b5642
Tomo Krajina <tkrajina@gmail.com>
2012-10-05 16:04:38 +0200

    DeliveryInfo -- added missing fields

--- oneapi/models/DeliveryInfo.class.php
+++ oneapi/models/DeliveryInfo.class.php
@@ -3,6 +3,9 @@
 class DeliveryInfo extends AbstractObject {
 
     public $deliveryStatus;
+    public $address;
+    public $messageId;
+    public $clientCorrelator;
 
     public function __construct() {
         parent::__construct();
