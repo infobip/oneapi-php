09386fec4a2c3531fee525604e384447e769f01b
Tomo Krajina <tkrajina@gmail.com>
2012-11-04 05:41:22 +0000

    Delivery push object example fix

--- oneapi-sample-app/on-inbound-message.php
+++ oneapi-sample-app/on-inbound-message.php
@@ -7,6 +7,14 @@ $result = SmsClient::unserializeInboundMessages();
 
 
 // Process the inbound message here...
+// ...
+
+
+// We'll just save this object:
+$fileName = PUSH_LOG_DIRECTORY . '/inbound-message-' . strftime('%Y-%m-%d %H:%M') . '.txt';
+$data = print_r($result, true);
+
+file_put_contents($fileName, $data);
 
 
 
@@ -20,13 +28,6 @@ $message->message = 'Thank you for your message (' . $result->message . ')!';
 $smsClient = new SmsClient(USERNAME, PASSWORD);
 
 $result = $smsClient->sendSMS($message);
-
-
-// We'll just save this object:
-$fileName = PUSH_LOG_DIRECTORY . '/inbound-message-' . strftime('%Y-%m-%d %H:%M') . '.txt';
-$data = print_r($result, true);
-
-file_put_contents($fileName, $data);
 
 
 // Not needed, but just for testing:
