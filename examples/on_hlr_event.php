2ac1905f16e2b950ec15d819b80a39350e53afce
frkator <isipka@infobip.com>
2012-12-04 15:39:45 +0100

    updated output

--- examples/on_hlr_event.php
+++ examples/on_hlr_event.php
@@ -18,6 +18,10 @@ fwrite($f, 'resourceURL: ' . $result->terminalRoamingStatus->resourceURL . "\n")
 fwrite($f, 'retrievalStatus: ' . $result->terminalRoamingStatus->retrievalStatus . "\n") ;
 fwrite($f, 'terminalRoamingStatus callbackData: ' . $result->terminalRoamingStatus->callbackData . "\n") ;
 fwrite($f, 'extendedData: ' . $result->terminalRoamingStatus->extendedData . "\n") ;
+fwrite($f, 'IMSI: ', $response->extendedData->imsi,'\n');
+fwrite($f, 'destinationAddres: ', $response->extendedData->destinationAddress,'\n');
+fwrite($f, 'originalNetworkPrefix: ', $response->extendedData->originalNetworkPrefix,'\n');
+fwrite($f, 'portedNetworkPrefix: ', $response->extendedData->portedNetworkPrefix,'\n');
 fwrite($f, "\n-------------------------------------\n");
 fclose($f);
 # ----------------------------------------------------------------------------------------------------
